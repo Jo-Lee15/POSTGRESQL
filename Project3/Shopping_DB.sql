5306e842b0e06679a3ffb5573e5279075c1db12d
Jo <Jlee.naidoo@gmail.com>
2022-07-28 13:24:14 +0200

    Shopping Cart DB

--- Project3/Shopping_DB.sql
+++ Project3/Shopping_DB.sql
@@ -20,7 +20,7 @@ SELECT * FROM product;
 
 CREATE TABLE cart (
    product_id bigint PRIMARY KEY NOT NULL,
-   quantity bigint NOT NULL
+   quantity bigint NOT NULL CHECK (quantity > -1)
 );
 
 --INNER JOIN
@@ -46,47 +46,27 @@ SELECT FROM cart;
 UPDATE cart SET cart.quantity = cart.quantity + 1 
 WHERE EXISTS(SELECT 1
              FROM cart
-             WHERE cart.product_id=1)
-             AND cart.product_id = 1;
-             
-
-UPDATE cart SET cart.quantity = cart.quantity + 1 
-WHERE EXISTS(SELECT 1
-             FROM cart
              WHERE cart.product_id=2)
              AND cart.product_id = 2;
-             
-UPDATE cart SET cart.quantity = cart.quantity + 1 
-WHERE EXISTS(SELECT 1
-             FROM cart
-             WHERE cart.product_id=3)
-             AND cart.product_id = 3;
-             
-UPDATE cart SET cart.quantity = cart.quantity + 1 
-WHERE EXISTS(SELECT 1
-             FROM cart
-             WHERE cart.product_id=4)
-             AND cart.product_id = 4;
-             
-UPDATE cart SET cart.quantity = cart.quantity + 1 
-WHERE EXISTS(SELECT 1
-             FROM cart
-             WHERE cart.product_id=5)
-             AND cart.product_id = 5; 
-             
+
+SELECT description, quantity, price, quantity*price AS subtotal FROM cart
+INNER JOIN product ON cart.product_id=product.product_id;
 
 --SHOW CART AFTER ADDITION
 
-SELECT description, quantity, price, quantity*price AS subtotal FROM cart
+SELECT product.product_id, description, quantity, price, quantity*price AS subtotal FROM cart
 INNER JOIN product ON cart.product_id=product.product_id;
 
 
 --IF QUANTITY = 0 ADD PRODUCT TO CART
 
-INSERT INTO cart (quantity)
-SELECT 1
+INSERT INTO cart (product_id, quantity)
+SELECT 2, 1
 WHERE NOT EXISTS (
-SELECT * FROM cart WHERE cart.product_id=5 AND cart.product_id = 5);
+SELECT 1 FROM cart WHERE product_id = 2);
+
+SELECT product.product_id, description, quantity, price, quantity*price AS subtotal FROM cart
+INNER JOIN product ON cart.product_id=product.product_id;
 
 
 --REMOVING AND ITEM FROM THE CART
@@ -94,33 +74,8 @@ SELECT * FROM cart WHERE cart.product_id=5 AND cart.product_id = 5);
 UPDATE cart SET cart.quantity = cart.quantity - 1 
 WHERE EXISTS(SELECT 1
              FROM cart
-             WHERE cart.product_id=1)
-             AND cart.product_id = 1;
-             
-
-UPDATE cart SET cart.quantity = cart.quantity - 1 
-WHERE EXISTS(SELECT 1
-             FROM cart
              WHERE cart.product_id=2)
              AND cart.product_id = 2;
-             
-UPDATE cart SET cart.quantity = cart.quantity - 1 
-WHERE EXISTS(SELECT 1
-             FROM cart
-             WHERE cart.product_id=3)
-             AND cart.product_id = 3;
-             
-UPDATE cart SET cart.quantity = cart.quantity - 1 
-WHERE EXISTS(SELECT 1
-             FROM cart
-             WHERE cart.product_id=4)
-             AND cart.product_id = 4;
-             
-UPDATE cart SET cart.quantity = cart.quantity - 1 
-WHERE EXISTS(SELECT 1
-             FROM cart
-             WHERE cart.product_id=5)
-             AND cart.product_id = 5; 
              
              
 --SHOW CART AFTER REMOVE
@@ -131,18 +86,18 @@ INNER JOIN product ON cart.product_id=product.product_id;
 
 --IF QUANTITY = 0 REMOVE PRODUCT TO CART
 
-DELETE FROM cart WHERE cart.quantity = 0;
+DELETE FROM cart WHERE cart.quantity <= 0;
 
 
 --CALCULATING THE SUBTOTAL
 
 SELECT description, quantity, price, quantity*price AS subtotal FROM cart
-INNER JOIN product ON cart.product_id=product.product_id;
+INNER JOIN product ON cart.product_id=product.product_id
 
 
 --CALCULATING THE GRAND TOTAL
 
-SELECT sum(cart.quantity*product.price) as grandtotal
+SELECT sum(cart.quantity*product.price) AS grandtotal
 FROM cart
-INNER JOIN product on product.product_id = cart.product_id;
+INNER JOIN product ON product.product_id = cart.product_id;
 
